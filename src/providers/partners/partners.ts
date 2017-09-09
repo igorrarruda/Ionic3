c536636ab495a1a9da8ce6b75f2f2b63c8d4b06a
unknown <redacademy@OFBDT035263.ffb.local>
2017-09-09 12:32:12 -0300

    Aula 3

--- src/providers/partners/partners.ts
+++ src/providers/partners/partners.ts
@@ -6,24 +6,28 @@ import { UserInfoProvider } from './../user-info/user-info';
 
 @Injectable()
 export class PartnersProvider {
-  api_url: string = 'https://cerveja-api.herokuapp.com';
+  api_url: string = 'https://cerveja-api.herokuapp.com/';
 
   constructor(
     public http: Http,
     private userInfo: UserInfoProvider
   ) { }
 
-  getIndexPartners() {
+  makeRequestOptions() {
     let headers = new Headers();
     headers.append('Content-type', 'application/x-www-urlencoded');
     headers.append('Access-token', this.userInfo.accessToken);
     headers.append('Uid', this.userInfo.email);
     headers.append('Client', this.userInfo.client);
 
-    let options = new RequestOptions({ headers: headers });
+    return new RequestOptions({ headers: headers });
+  }
+
+  getIndexOfPartners(page: number) {
+    let options = this.makeRequestOptions();
 
     return new Promise((resolve, reject) => {
-      this.http.get(this.api_url + '/partners', options)
+      this.http.get(this.api_url + 'partners?page=' + page, options)
         .map(response => response.json())
         .subscribe(data => {
           resolve(data);
@@ -31,5 +35,19 @@ export class PartnersProvider {
           reject(err);
         });
     })
+  }
+
+  showPartner(id) {
+    let options = this.makeRequestOptions();
+
+    return new Promise((resolve, reject) => {
+      this.http.get(this.api_url + 'partners/' + id, options)
+        .map(res => res.json())
+        .subscribe(data => {
+          resolve(data);
+        }, (err) => {
+          reject(err)
+        });
+    });
   }
 }
